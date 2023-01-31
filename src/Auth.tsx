4c5d0b3d4cf7c131a8e622b1f0107458f3f2d389
avijeet <avijeetfly143@gmail.com>
2023-01-31 11:30:01 +0530

    SignUp code added.

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -6,6 +6,7 @@ const Auth: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<any>(null);
+  const [active, setActive] = useState(false);
 
   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -27,16 +28,36 @@ const Auth: React.FC = () => {
 
   return (
     <div className="auth">
-      <form className="auth-form" onSubmit={handleLogin}>
-        <input type="email" placeholder="Email id" value={email} onChange={(event) => setEmail(event.target.value)} />
-        <input
-          type="password"
-          value={password}
-          placeholder="Password"
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
+      {active ? (
+        <form className="auth-form" onSubmit={handleSignUp}>
+          <input type="email" placeholder="Email id" value={email} onChange={(event) => setEmail(event.target.value)} />
+          <input
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button type="submit">SignUp</button>
+          <div onClick={() => setActive(!active)} className="activeBtn">
+            Already user LogIn
+          </div>
+        </form>
+      ) : (
+        <form className="auth-form" onSubmit={handleLogin}>
+          <input type="email" placeholder="Email id" value={email} onChange={(event) => setEmail(event.target.value)} />
+          <input
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button type="submit">Login</button>
+          <div onClick={() => setActive(!active)} className="activeBtn">
+            Don't have account signUp
+          </div>
+        </form>
+      )}
+
       {error && <p className="auth-error">{error.message}</p>}
     </div>
   );
